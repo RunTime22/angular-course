7c7c47784d51a06fb345c240e1b9428a9938a278
Hitmands <gius.mand.developer@gmail.com>
2015-03-26 17:15:43 +0100

    done slides

--- src/slides/snippets/1-angular.module.js
+++ src/slides/snippets/1-angular.module.js
@@ -195,25 +195,44 @@
 
 })();
 
-
-
     angular
       .module('esis.angular-course.snippets')
-      .filter('myCustomFilter', function myCustomFilterFactory( /* injectables */ ) {
-        return function myCustomFilterWorker( dataToFormat, param, paramN ) {
-          var formattedData;
-
-          // Filters can break app!
-          if(!angular.isArray(dataToFormat)) {
-            return dataToFormat;
+      .directive('myDir', function( /* injectables */ ) {
+        // Use default DDO props
+        return function postLink(iScope, iElement, iAttrs) {
+          // Executed when template is compiled and the scope is linked.
+        };
+      })
+      .directive('myDir', function( /* injectables */ ) {
+        return /* DDO */ {
+          restrict: 'A', // EACM
+          template: '', // or function(tEl, tAttr)
+          // or templateUrl: '', // or function(tEl, tAttr)
+          priority: 0,
+          transclude: false,
+          templateNamespace: 'html',
+          scope: false, // true or Object
+          controller: function($scope, $element, $attrs, $transclude, otherInjectables) {  },
+          controllerAs: 'stringIdentifier',
+          bindToController: false,
+          require: 'siblingDirectiveName', // or // ['^parentDirectiveName', '?optionalDirectiveName', '?^optionalParent'],
+          compile: function compile(tElement, tAttrs, transclude) {
+            return {
+              pre: function preLink(scope, iElement, iAttrs, controller) {  },
+              post: function postLink(scope, iElement, iAttrs, controller) {  }
+            };
+            // or
+            // return function postLink( ... ) { ... }
           }
-
-          formattedData = dataToFormat.shift();
-
-
-          return formattedData;
-        }
-    });
+          // or
+          // link: {
+          //  pre: function preLink(scope, iElement, iAttrs, controller) { ... },
+          //  post: function postLink(scope, iElement, iAttrs, controller) { ... }
+          // }
+          // or
+          // link: function postLink( ... ) { ... }
+        };
+      });
 
 
 
